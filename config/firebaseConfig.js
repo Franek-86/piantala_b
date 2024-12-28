a20214762be0296b3d5b224f1184445069aac229
Franek-86 <fraleccese86@gmail.com>
2024-12-28 20:57:16 +0100

    test

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,7 +2,7 @@ var admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.BUCKET_NAME,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 const bucket = admin.storage().bucket();
 
