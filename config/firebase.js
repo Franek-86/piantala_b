28ebeedf472e43046c238d46f7dfcbaf3d23c046
Franek-86 <fraleccese86@gmail.com>
2024-12-28 19:49:42 +0100

    test

--- config/firebase.js
+++ config/firebase.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDEER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 
   appId: process.env.FIREBASE_APP_ID,
 
