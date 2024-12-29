369ef686f5dabf7fa754b4e7586518b45ab8fb12
Franek-86 <fraleccese86@gmail.com>
2024-12-29 10:37:14 +0100

    test

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -7,5 +7,19 @@ admin.initializeApp({
 console.log("qui", serviceAccount);
 const bucket = admin.storage().bucket();
 console.log("Firebase bucket initialized:", bucket.name); // Ensure bucket name is correct
-
+console.log(
+  "Firebase Service Account Initialized:",
+  serviceAccount.client_email
+);
+async function testFirebaseConnection() {
+  try {
+    const [files] = await bucket.getFiles({ maxResults: 1 }); // Try listing files in the bucket
+    console.log(
+      "Connection to Firebase successful. Files in bucket:",
+      files.length > 0 ? files.map((f) => f.name) : "No files found."
+    );
+  } catch (error) {
+    console.error("Failed to connect to Firebase Storage:", error.message);
+  }
+}
 module.exports = bucket;
