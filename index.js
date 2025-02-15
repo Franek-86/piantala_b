487bbe2cea39374143dc9a9f683c570660bf4f6f
Franek-86 <fraleccese86@gmail.com>
2025-02-15 18:42:59 +0100

    test

--- index.js
+++ index.js
@@ -27,7 +27,22 @@ app.options("*", cors());
 app.use("/uploads", express.static("uploads"));
 
 app.use(bodyParser.json());
+const { exec } = require("child_process");
 
+exec(
+  'curl -s "http://api.geonames.org/childrenJSON?geonameId=3182350&username=franek"',
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Curl Response:\n${stdout}`);
+  }
+);
 // const sessionStore = new MySQLStore({}, require("./config/db"));
 const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 // app.use(
