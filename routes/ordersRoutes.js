4c7375598f261aa49d0ab5538012795e3c584065
Franek-86 <fraleccese86@gmail.com>
2025-08-12 18:48:24 +0200

    test

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const ordersController = require("../controllers/ordersControllers");
-
+const isAuth = require("../middleware/isAuth");
 router.post("/add-order", ordersController.addOrder);
 router.get("/all-orders", ordersController.getOrders);
-router.patch("/update-order", ordersController.updateOrder);
+router.patch("/update-order", isAuth, ordersController.updateOrder);
 module.exports = router;
