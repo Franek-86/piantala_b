ce43a76fdb7307899488e1ec019a8ec587a5f7c0
Franek-86 <fraleccese86@gmail.com>
2025-08-27 18:06:23 +0200

    test

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -2,14 +2,15 @@ const db = require("../models");
 const express = require("express");
 const app = express();
 exports.message = async (req, res) => {
-  console.log(req.body);
-  const { sender_id, receiver_id, content } = req.body;
+  console.log("q123", req.body);
+  const { sender_id, sender_username, receiver_id, content } = req.body;
   const newMessage = await db.Message.create({
     sender_id,
     receiver_id,
     content,
+    sender_username,
   });
-  console.log(newMessage.dataValues.content);
+  console.log("q123", newMessage.dataValues);
   const data = newMessage.dataValues;
   if (newMessage) {
     res.status(200).send("message successfully posted");
--- models/Messages.js
+++ models/Messages.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   Messages.init(
     {
       sender_id: DataTypes.INTEGER,
+      sender_username: DataTypes.STRING,
       receiver_id: DataTypes.INTEGER,
       content: DataTypes.STRING,
     },
