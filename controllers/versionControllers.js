fbe68d1a5f7046b177838ed10c3bbd3e50ed98a7
Franek-86 <fraleccese86@gmail.com>
2026-03-11 12:32:36 +0100

    test

--- controllers/versionControllers.js
+++ controllers/versionControllers.js
@@ -1,8 +1,17 @@
+const db = require("../models");
+const Version = db.Version;
+
 exports.version = async (req, res) => {
-  const version = process.env.CURRENT_VERSION;
+  // console.log("qui", Version);
+  // const version = process.env.CURRENT_VERSION;
   try {
-    await res.json({ message: { version_number: version }, status: 200 });
+    const response = await Version.findAll({});
+    let version_number = response[0].dataValues.version;
+
+    await res.json({ message: { version_number }, status: 200 });
   } catch (err) {
+    console.log(err);
+    // await res.json({ message: { version_number: "0.0.1" }, status: 200 });
     await res.json({ message: { "error from catch": err }, status: 500 });
   }
 };
---
+++ models/Version.js
@@ -0,0 +1,25 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Version extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Version.init(
+    {
+      version: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Version",
+      tableName: "version",
+    },
+  );
+  return Version;
+};
